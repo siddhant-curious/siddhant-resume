ced797bcf927e35571eb17794d9666fb7235a4ab
siddhant <siddhantchhabra96@gmail.com>
2018-04-01 14:03:53 +0530

    Optimised For Web

--- index.py
+++ index.py
@@ -1,7 +1,11 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template,redirect, url_for
 app = Flask(__name__)
 
 @app.route('/')
 def index():
 	return render_template('resume.html')
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return redirect(url_for('index'))
